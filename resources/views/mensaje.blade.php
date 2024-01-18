e3a80984106a47bbb25212b7d3285a396e4284c3
Georgina <georginaalc4010@gmail.com>
2024-01-18 13:41:50 -0600

    m2

--- resources/views/mensaje.blade.php
+++ resources/views/mensaje.blade.php
@@ -139,7 +139,7 @@
 			<div class="container">
 				<article class="page-article" itemprop="blogPost">
 					<h1 itemprop="about">MENSAJE DEL DIRECTOR</h1>
-					
+
 					<img itemprop="image" src="images/top-college-pic.jpg" alt="Image">
 					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
@@ -153,163 +153,10 @@
 					</div>
 				</article>
 
-				<aside>
-					<div class="categories-block">
-						<h2>categories</h2>
-					</div>
-					<div class="recent-post">
-						<h2>recent posts</h2>
-						<div class="post">
-							<div class="post-wrap">
-								<div class="img-wrap">
-									<img src="images/recent-post-img.jpg" alt="Post Images">
-								</div>
-								<a href="#"><div class="post-content">
-									<h3>How to find best college for you..</h3>
-									<p>26 Dec, 2018</p>
-								</div></a>
-							</div>
-
-							<div class="post-wrap">
-								<div class="img-wrap">
-									<img src="images/recent-post-img.jpg" alt="Post Images">
-								</div>
-								<a href="#"><div class="post-content">
-									<h3>How to find best college for you..</h3>
-									<p>26 Dec, 2018</p>
-								</div></a>
-							</div>
-
-							<div class="post-wrap">
-								<div class="img-wrap">
-									<img src="images/recent-post-img.jpg" alt="Post Images">
-								</div>
-								<a href="#"><div class="post-content">
-									<h3>How to find best college for you..</h3>
-									<p>26 Dec, 2018</p>
-								</div></a>
-							</div>
-
-							<div class="post-wrap">
-								<div class="img-wrap">
-									<img src="images/recent-post-img.jpg" alt="Post Images">
-								</div>
-								<a href="#"><div class="post-content">
-									<h3>How to find best college for you..</h3>
-									<p>26 Dec, 2018</p>
-								</div></a>
-							</div>
-						</div>
-					</div>
-					<!-- Recent Post Close -->
-				</aside>
-				<!--<section class="comment-section">
-					<div id="comments" class="comments-area comment" itemprop="comment">
-								<h3 class="comments-title">8 Comments</h3>
-								<ol class="comment-list">
-									<li class="comment even thread-even depth-1 parent">
-										<article class="comment-body">
-											<footer class="comment-meta">
-												<div class="comment-author vcard">
-													<img class="avatar photo" src="images/comment-img1.jpg" alt="">
-													<b class="fn"><a href="#">Michael</a></b>
-												</div>
-												<div class="comment-metadata">
-													<a href="#"><time datetime="2013-10-02">October 09, 2015</time></a>
-												</div><br>
-											</footer>
-											<div class="comment-content">
-												<p>Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum.</p>
-											</div>
-											<div class="reply">
-												<a href="#" class="comment-reply-link">Reply</a>
-											</div>
-										</article>
-										<ol class="children">
-											<li class="comment odd alt depth-2 parent">
-												<article class="comment-body">
-													<footer class="comment-meta">
-														<div class="comment-author vcard">
-															<img class="avatar photo" src="images/comment-img2.jpg" alt="">
-															<b class="fn"><a href="#">John Doe</a></b>
-														</div>
-														<div class="comment-metadata">
-															<a href="#"><time datetime="2013-10-02">October 09, 2015</time></a>
-														</div><br>
-													</footer>
-													<div class="comment-content">
-														<p>Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. </p>
-													</div>
-													<div class="reply">
-														<a href="#" class="comment-reply-link">Reply</a>
-													</div>
-												</article>
-												<ol class="children">
-													<li class="comment odd alt depth-2 parent">
-														<article class="comment-body">
-															<footer class="comment-meta">
-																<div class="comment-author vcard">
-																	<img class="avatar photo" src="images/comment-img3.jpg" alt="">
-																	<b class="fn"><a href="#">John Doe</a></b>
-																</div>
-																<div class="comment-metadata">
-																	<a href="#"><time datetime="2013-10-02">October 09, 2015</time></a>
-																</div><br>
-															</footer>
-															<div class="comment-content">
-																<p>Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. </p>
-															</div>
-															<div class="reply">
-																<a href="#" class="comment-reply-link">Reply</a>
-															</div>
-														</article>
-													</li>
-												</ol>
-											</li>
-										</ol>
-									</li>
-									<li class="comment even thread-even depth-1 parent">
-										<article class="comment-body">
-											<footer class="comment-meta">
-												<div class="comment-author vcard">
-													<img class="avatar photo" src="images/comment-img4.jpg" alt="">
-													<b class="fn"><a href="#">Hilco de Roo</a></b>
-												</div>
-												<div class="comment-metadata">
-													<a href="#"><time datetime="2013-10-02">October 09, 2015</time></a>
-												</div><br>
-											</footer>
-											<div class="comment-content">
-												<p>Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. </p>
-											</div>
-											<div class="reply">
-												<a href="#" class="comment-reply-link">Reply</a>
-											</div>
-										</article>
-									</li>
-								</ol>
-					</div>
-					<div class="comment-form">
-						<h2>leave a comment</h2>
-						<form>
-							<input type="text" placeholder="Username*">
-							<input type="email" placeholder="Email*">
-							<textarea placeholder="Write a comment.....">
-							</textarea>
-							<input type="submit" value="Submit">
-						</form>
-						<p>Note: Your email address will not be published</p>
-					</div>
-				</section> -->
+				
 			</div>
 		</div>
-		<!-- Page-content closed -->
-		<section class="query-section">
-			<div class="container">
-				<p>Any Queries? Ask us a question at<a href="tel:+9779813639131"><i class="fas fa-phone"></i> +977 9813639131</a></p>
-			</div>
-		</section>
-		<!-- End of Query Section -->
+
 		<footer class="page-footer">
 			<div class="footer-first-section">
 				<div class="container">
